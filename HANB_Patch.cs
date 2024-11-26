582ad730b57c88dc3a772c499bfee304104e32dd
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-11-26 09:17:33 +0100

    Cleanup

--- HANB_Patch.cs
+++ HANB_Patch.cs
@@ -5,29 +5,29 @@ using System.Reflection;
 
 namespace HandsAreNotBusy
 {
-    internal class HANB_Patch : ModulePatch
-    {
-        protected override MethodBase GetTargetMethod()
-        {
-            return typeof(GameWorld).GetMethod(nameof(GameWorld.RegisterPlayer));
-        }
+	internal class HANB_Patch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(GameWorld).GetMethod(nameof(GameWorld.RegisterPlayer));
+		}
 
-        [PatchPostfix]
-        public static void PostFix(IPlayer iPlayer)
-        {
-            if (iPlayer == null)
-            {
-                HANB_Plugin.HANB_Logger.LogError("Could not add component, player was null!");
-                return;
-            }
+		[PatchPostfix]
+		public static void PostFix(IPlayer iPlayer)
+		{
+			if (iPlayer == null)
+			{
+				HANB_Plugin.HANB_Logger.LogError("Could not add component, player was null!");
+				return;
+			}
 
-            if (!iPlayer.IsYourPlayer)
-            {
-                return;
-            }
+			if (!iPlayer.IsYourPlayer)
+			{
+				return;
+			}
 
-            Singleton<GameWorld>.Instance.MainPlayer.gameObject.AddComponent<HANB_Component>();
-            HANB_Plugin.HANB_Logger.LogInfo("Added HANB Component to player: " + Singleton<GameWorld>.Instance.MainPlayer.Profile.Nickname);
-        }
-    }
+			Singleton<GameWorld>.Instance.MainPlayer.gameObject.AddComponent<HANB_Component>();
+			HANB_Plugin.HANB_Logger.LogInfo("Added HANB Component to player: " + Singleton<GameWorld>.Instance.MainPlayer.Profile.Nickname);
+		}
+	}
 }
