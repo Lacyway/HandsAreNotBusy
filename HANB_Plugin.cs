4f3616242f1671fa4f42e7881a185a9e50570f6a
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-10-11 09:56:59 +0200

    4.0

--- HANB_Plugin.cs
+++ HANB_Plugin.cs
@@ -3,21 +3,20 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using UnityEngine;
 
-namespace HandsAreNotBusy
+namespace HandsAreNotBusy;
+
+[BepInPlugin("com.lacyway.hanb", "HandsAreNotBusy", "1.6.0")]
+internal class HANB_Plugin : BaseUnityPlugin
 {
-	[BepInPlugin("com.lacyway.hanb", "HandsAreNotBusy", "1.5.0")]
-	internal class HANB_Plugin : BaseUnityPlugin
-	{
-		internal static ConfigEntry<KeyboardShortcut> ResetKey;
-		internal static ManualLogSource HANB_Logger;
+    internal static ConfigEntry<KeyboardShortcut> ResetKey;
+    internal static ManualLogSource HANB_Logger;
 
-		protected void Awake()
-		{
-			ResetKey = Config.Bind("Keybind", "Reset Key", new KeyboardShortcut(KeyCode.End), new ConfigDescription("The key to reset the broken controller with."));
-			HANB_Logger = Logger;
+    protected void Awake()
+    {
+        ResetKey = Config.Bind("Keybind", "Reset Key", new KeyboardShortcut(KeyCode.End), new ConfigDescription("The key to reset the broken controller with."));
+        HANB_Logger = Logger;
 
-			HANB_Logger.LogInfo($"{nameof(HANB_Plugin)} has been loaded.");
-			new HANB_Patch().Enable();
-		}
-	}
+        HANB_Logger.LogInfo($"{nameof(HANB_Plugin)} has been loaded.");
+        new HANB_Patch().Enable();
+    }
 }
